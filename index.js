1bf7eda7d59ce48774310e1d83917fc80689d94c
ilsken <chris@ilsken.com>
2013-12-19 15:20:46 -0500

    Cleaned up tabs, removed value function (leave that to higher level libraries)

--- index.js
+++ index.js
@@ -4,37 +4,23 @@ var slice = Array.prototype.slice
  * @params {...} Objects to use as the lookup chain, first objects have higher precedence
  */
 function Scope(){
-	var self = this instanceof Scope 
-		? this 
-		: Object.create(Scope.prototype)
+  var self = this instanceof Scope 
+    ? this 
+    : Object.create(Scope.prototype)
 
-	self.scopes = slice.call(arguments)
-	self.regenerate()
-	// factory method
-	if(self !== this) return self
-}
-
-/**
- * Returns the value of a property, passed to views
- * @param  {String} prop Property name
- * @return {Mixed}      
- */
-Scope.prototype.value = function(prop){
-	var scope = this.$lookup(prop)
-	if(typeof scope[prop] == 'function'){
-		return scope[prop]();
-	} else{
-		return scope[prop]
-	}
+  self.scopes = slice.call(arguments)
+  self.regenerate()
+  // factory method
+  if(self !== this) return self
 }
 
 /**
  * Finds the scope that contains the specified property
  * @param  {String} prop Property name
  * @return {Object}      Scope that contains the property
- */	
+ */ 
 Scope.prototype.lookup = function(prop){
-	return this.$lookup(prop)
+  return this.$lookup(prop)
 }
 
 /**
@@ -43,7 +29,7 @@ Scope.prototype.lookup = function(prop){
  * @return {[type]}      [description]
  */
 Scope.prototype.get = function(prop){
-	return this.$lookup(prop)[prop]
+  return this.$lookup(prop)[prop]
 }
 
 /**
@@ -51,7 +37,7 @@ Scope.prototype.get = function(prop){
  * @return {[type]} [description]
  */
 Scope.prototype.regenerate = function(){
-	this.$lookup = _lookup(this.scopes)
+  this.$lookup = _lookup(this.scopes)
 }
 
 /*!
@@ -59,11 +45,11 @@ Scope.prototype.regenerate = function(){
  * @private
  */
 function _lookup(scopes){
-	scopes = Array.isArray(scopes) ? scopes : [undefined]
+  scopes = Array.isArray(scopes) ? scopes : [undefined]
 
-	var body = scopes.map(function(scope, i){
-		scope = '$scope[' + i + ']';
-		return 'if(' + scope +'.hasOwnProperty(prop)) return ' + scope;
-	}).join(';\n')
-	return new Function('$scope', 'return function(prop){  ' + body + ' }')(scopes)
+  var body = scopes.map(function(scope, i){
+    scope = '$scope[' + i + ']';
+    return 'if(' + scope +'.hasOwnProperty(prop)) return ' + scope;
+  }).join(';\n')
+  return new Function('$scope', 'return function(prop){  ' + body + ' }')(scopes)
 }
